80bec507f0575a7ea72f803a4bb193c900b7cbe6
yeasinmahi <yeasinmahi72@yahoo.com>
2017-01-21 01:22:53 +0600

    adding side panel

--- app/src/main/java/com/gits/sami/billmanagement/activity/MainActivity.java
+++ app/src/main/java/com/gits/sami/billmanagement/activity/MainActivity.java
@@ -2,13 +2,20 @@ package com.gits.sami.billmanagement.activity;
 
 import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -19,8 +26,10 @@ import com.gits.sami.billmanagement.db.PopulatedOpenHelper;
 import com.gits.sami.billmanagement.fragments.DatePickerFragment;
 import com.gits.sami.billmanagement.fragments.ElectricityFragment;
 import com.gits.sami.billmanagement.fragments.GasFragment;
+import com.gits.sami.billmanagement.fragments.ReportsFragment;
 import com.gits.sami.billmanagement.fragments.WasaFragment;
 import com.gits.sami.billmanagement.others.Utility.dateEnum;
+import com.gits.sami.billmanagement.others.ViewPagerAdapter;
 
 import java.text.DateFormat;
 import java.util.ArrayList;
@@ -28,14 +37,20 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, NavigationView.OnNavigationItemSelectedListener{
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Init();
+        InitNavigation();
         PopulatedOpenHelper.getInstance(getApplicationContext());
+    }
+
+    private void InitNavigation() {
+
+
     }
 
     private void Init() {
@@ -50,6 +65,15 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
 
@@ -58,6 +82,7 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
         adapter.addFragment(new ElectricityFragment(), "Electricity");
         adapter.addFragment(new WasaFragment(), "Wasa");
         adapter.addFragment(new GasFragment(), "Gas");
+        adapter.addFragment(new ReportsFragment(), "Reports");
         viewPager.setAdapter(adapter);
     }
     dateEnum bill;
@@ -91,32 +116,43 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
         setDate(calendar);
     }
 
-    class ViewPagerAdapter extends FragmentPagerAdapter {
-        private final List<Fragment> mFragmentList = new ArrayList<>();
-        private final List<String> mFragmentTitleList = new ArrayList<>();
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
 
-        public ViewPagerAdapter(FragmentManager manager) {
-            super(manager);
+        if (id == R.id.nav_electricity) {
+            moveToFragment(new ElectricityFragment());
+        } else if (id == R.id.nav_wasa) {
+            moveToFragment(new WasaFragment());
+        } else if (id == R.id.nav_gas) {
+
+        } else if (id == R.id.nav_reports) {
+
+        } else if (id == R.id.nav_setting) {
+
+        } else if (id == R.id.nav_about) {
+
         }
 
-        @Override
-        public Fragment getItem(int position) {
-            return mFragmentList.get(position);
-        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    private void moveToFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.viewpager, fragment, "createPost").addToBackStack(null).commit();
 
-        @Override
-        public int getCount() {
-            return mFragmentList.size();
-        }
-
-        public void addFragment(Fragment fragment, String title) {
-            mFragmentList.add(fragment);
-            mFragmentTitleList.add(title);
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return mFragmentTitleList.get(position);
+    }
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
     }
+
+
 }
---
+++ app/src/main/java/com/gits/sami/billmanagement/fragments/ReportsFragment.java
@@ -0,0 +1,33 @@
+package com.gits.sami.billmanagement.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.gits.sami.billmanagement.R;
+
+
+public class ReportsFragment extends Fragment {
+
+    public ReportsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_reports, container, false);
+    }
+
+
+}
